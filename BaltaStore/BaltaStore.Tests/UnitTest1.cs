3c91e2e232642c1f632f3470c67dac8e80d3186a
rangel <daykes@gmail.com>
2019-08-02 16:50:24 -0300

    Tests

--- BaltaStore/BaltaStore.Tests/UnitTest1.cs
+++ BaltaStore/BaltaStore.Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using BaltaStore.Domain.StoreContext.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BaltaStore.Tests
@@ -8,6 +9,14 @@ namespace BaltaStore.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            var c = new Customer(
+                "José",
+                "Lelé",
+                "32065432180",
+                "lele@gmail.com",
+                "9898989898",
+                "Rua vai e vem, 123"
+            );            
         }
     }
 }
