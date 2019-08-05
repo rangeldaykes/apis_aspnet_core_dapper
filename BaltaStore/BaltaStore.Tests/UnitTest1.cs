8200b69e5e476d39cfa2402fbec39f61331baedc
rangel <daykes@gmail.com>
2019-08-05 06:23:42 -0300

    Simulando o Pedido

--- BaltaStore/BaltaStore.Tests/UnitTest1.cs
+++ BaltaStore/BaltaStore.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using BaltaStore.Domain.StoreContext.Entities;
+using BaltaStore.Domain.StoreContext.ValueObjects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BaltaStore.Tests
@@ -9,18 +10,34 @@ namespace BaltaStore.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            var c = new Customer(
-                "José",
-                "Lelé",
-                "32065432180",
-                "lele@gmail.com",
-                "9898989898",
-                "Rua vai e vem, 123"
-            );            
+            var name = new Name("José", "Lelé");
+            var document = new Document("32065432180");
+            var email = new Email("lele@gmail.com");
+            var c = new Customer(name, document, email, "9898989898");
 
-            var order = new Order(c);
-            //order.AddItem()
+            var mouse = new Product("Mouse", "Rato", "Image.png", 59.90M, 10);
+            var teclado = new Product("Teclado", "Teclado", "Image.png", 159.90M, 10);
+            var impressora = new Product("Impressora", "Impressora", "Image.png", 359.90M, 10);
+            var cadeira = new Product("Cadeira", "Cadeira", "Image.png", 559.90M, 10);
+            
+            var order = new Order(c);            
+            order.AddItem(new OrderItem(mouse, 5));
+            order.AddItem(new OrderItem(teclado, 5));
+            order.AddItem(new OrderItem(cadeira, 5));
+            order.AddItem(new OrderItem(impressora, 5));
 
+            // Realizei o pedido
+            order.Place();
+
+            // Simular pagamento
+            order.Pay();
+
+            // Simular Envio
+            order.Ship();
+
+            // Simular o cancelamento
+            order.Cancel();
+            
         }
     }
 }
