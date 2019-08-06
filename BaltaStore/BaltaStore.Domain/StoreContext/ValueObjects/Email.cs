4607d616a9ed02cc15eb7761549ce5a797bb378e
rangel <daykes@gmail.com>
2019-08-06 16:29:22 -0300

    Implementando Notificacoes

--- BaltaStore/BaltaStore.Domain/StoreContext/ValueObjects/Email.cs
+++ BaltaStore/BaltaStore.Domain/StoreContext/ValueObjects/Email.cs
@@ -1,10 +1,18 @@
+using Flunt.Notifications;
+using Flunt.Validations;
+
 namespace BaltaStore.Domain.StoreContext.ValueObjects
 {
-    public class Email
+    public class Email : Notifiable
     {
         public Email(string address)
         {
             Address = address;
+
+            AddNotifications(new Contract()
+                .Requires()
+                .IsEmail(Address, "Email", "O E-mail é inválido")
+            );
         }
 
         public string Address { get; private set; }
--- BaltaStore/BaltaStore.Domain/StoreContext/ValueObjects/Name.cs
+++ BaltaStore/BaltaStore.Domain/StoreContext/ValueObjects/Name.cs
@@ -10,13 +10,19 @@ namespace BaltaStore.Domain.StoreContext.ValueObjects
             FirstName = firstName;
             LastName = lastName;
 
-            new Contract().Requires().HasMinLen(FirstName, 3, "FirstName", "O nome deve conter pelo menos 3 caracteres");
+            AddNotifications(new Contract()
+                .Requires()
+                .HasMinLen(FirstName, 3, "FirstName", "O nome deve conter pelo menos 3 caracteres")
+                .HasMaxLen(FirstName, 40, "FirstName", "O nome deve conter no máximo 40 caracteres")
+                .HasMinLen(LastName, 3, "LastName", "O sobrenome deve conter pelo menos 3 caracteres")
+                .HasMaxLen(LastName, 40, "LastName", "O sobrenome deve conter no máximo 40 caracteres")
+            );
         }
 
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
 
-        public override string ToString() 
+        public override string ToString()
         {
             return $"{FirstName} {LastName}";
         }
