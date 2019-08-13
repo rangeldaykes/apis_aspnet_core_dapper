5e6df77291ce5dfd51e0c991084a0e5e7e0d59e7
rangel <daykes@gmail.com>
2019-08-13 17:07:10 -0300

    commands folders

---
+++ BaltaStore/BaltaStore.Domain/StoreContext/Commands/CustomerCommands/Inputs/AddAddressCommand.cs
@@ -0,0 +1,18 @@
+using BaltaStore.Domain.StoreContext.Enums;
+
+namespace BaltaStore.Domain.StoreContext.CustomerCommands.Inputs
+{
+    public class AddAddressCommand
+    {
+        public Guid Id { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string Complement { get; set; }
+        public string Distrinct { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+        public EAddressType Type { get; set; }
+    }
+}
